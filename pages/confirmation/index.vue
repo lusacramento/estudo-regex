4164f4fe1071a15ee6d6d3dacf7d3e1b97563ce1
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-01-05 12:27:57 -0300

    first version

---
+++ pages/confirmation/index.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="text-center">
+		<h2>Obrigado por utilizar nosso app!</h2>
+		<a href="/">
+			<button class="btn btn-warning mt-3">Voltar</button>
+		</a>
+	</div>
+</template>
+<style>
+	html {
+		height: 99vw;
+		display: flex;
+		justify-content: center;
+	}
+	body {
+		display: flex;
+		align-items: center;
+	}
+</style>
