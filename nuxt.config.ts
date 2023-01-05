648d6365ce5ef578c4c3c3575fb8e9c9f0b869df
Lu Sacramento <lucianossacramento@yahoo.com.br>
2023-01-05 16:08:57 -0300

    fix boostrap js files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['/assets/styles/main.scss'],
 	ssr: false,
+	css: ['/assets/styles/main.scss'],
+	app: {
+		head: {
+			script: [
+				{
+					src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js',
+					type: 'text/javascript',
+				},
+			],
+		},
+	},
+	vite: {
+		define: {
+			'process.env.DEBUG': false,
+		},
+	},
 })
